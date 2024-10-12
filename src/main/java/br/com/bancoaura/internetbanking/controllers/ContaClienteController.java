3d1d9f7f9d478492b19c4e3397cbe7cc4176f6aa
Helena Bispo <bispohelena06@gmail.com>
2024-10-12 15:35:08 -0300

    Adiciona classes de controllers para Autenticacao, Conta do cliente e Autenticacao

---
+++ src/main/java/br/com/bancoaura/internetbanking/controllers/ContaClienteController.java
@@ -0,0 +1,10 @@
+package br.com.bancoaura.internetbanking.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/contas")
+public class ContaClienteController {
+
+}

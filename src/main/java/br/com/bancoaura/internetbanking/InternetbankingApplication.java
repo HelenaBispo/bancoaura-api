95a5713e85c5686a4c1ed7cb21cb55833a1dd319
Helena Bispo <bispohelena06@gmail.com>
2024-10-12 15:41:04 -0300

    Adiciona configuração de fuso-horário para America/Recife

--- src/main/java/br/com/bancoaura/internetbanking/InternetbankingApplication.java
+++ src/main/java/br/com/bancoaura/internetbanking/InternetbankingApplication.java
@@ -3,10 +3,13 @@ package br.com.bancoaura.internetbanking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InternetbankingApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Recife"));
 		SpringApplication.run(InternetbankingApplication.class, args);
 	}
 

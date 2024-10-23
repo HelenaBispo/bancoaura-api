9a4902173279e3ec6e1b021652be205fe11c510f
Helena Bispo <bispohelena06@gmail.com>
2024-10-22 21:54:12 -0300

    Adiciona controller responsável pela autenticação na API

--- src/main/java/br/com/bancoaura/internetbanking/controllers/AutenticacaoController.java
+++ src/main/java/br/com/bancoaura/internetbanking/controllers/AutenticacaoController.java
@@ -1,9 +1,35 @@
 package br.com.bancoaura.internetbanking.controllers;
 
+import br.com.bancoaura.internetbanking.dtos.ContaDto;
+import br.com.bancoaura.internetbanking.servicos.AutenticacaoService;
+import br.com.bancoaura.internetbanking.servicos.ContaClienteService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
-@RequestMapping("/api/autenticacao")
+@RequestMapping("/api")
 public class AutenticacaoController {
+
+    @Autowired
+    AutenticacaoService autenticacaoService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> fazerLogin(@RequestBody ContaDto contaDto) {
+        try {
+            ContaDto conta = autenticacaoService.autenticar(contaDto.getId(), contaDto.getSenha());
+
+            return ResponseEntity.ok().body(conta);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
 }
